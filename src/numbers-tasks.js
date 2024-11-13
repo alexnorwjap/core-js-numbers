6f7c7170cc287abbe557de05aae5403d2fe41b90
Aleks D.V. <alex.norw.jap@gmail.com>
2024-11-13 23:41:55 +0300

    feat: Add functions
    roundToLargestInteger
    roundToNearestInteger
    getIntegerPartNumber
    getSumOfNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -517,8 +517,8 @@ function roundToSmallestInteger(number) {
  * 5.1  => 6
  * -5.9 => -5
  */
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
@@ -532,8 +532,8 @@ function roundToLargestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
@@ -547,8 +547,8 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**
@@ -563,8 +563,8 @@ function getIntegerPartNumber(/* number */) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(...rest) {
+  return rest.reduce((acc, count) => acc + count).toFixed(2);
 }
 
 /**
